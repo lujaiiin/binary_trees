650e2e7a29e33f909aaf9a5eb95eba85441d88ed
lujain <lujainm.com>
2024-01-04 13:07:15 -0800

    New node

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling
+ * @node: val
+ * Return: always
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+	{
+		return (NULL);
+	}
+	if (node->parent->left == node)
+	{
+		return (node->parent->right);
+	}
+	return (node->parent->left);      
+}
