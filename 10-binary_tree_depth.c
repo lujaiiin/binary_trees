4583852dc70de117ef56ac7cb7d563a9118c1363
lujain <lujainm.com>
2024-01-04 10:14:37 -0800

    New node

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -13,7 +13,8 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		for(t = tree->parent; t; d++, t = t->parent);
+		for (t = tree->parent; t; d++, t = t->parent)
+			;
 		return (d);
 	}
 
