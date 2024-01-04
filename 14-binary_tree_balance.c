fa22424065d669d3e24b072088679111c450a4dd
lujain <lujainm.com>
2024-01-04 12:45:18 -0800

    New node

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,7 +9,7 @@
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree != NULL)
-		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+		return (binary_tree_he(tree->left) - binary_tree_he(tree->right));
 	return (0);
 }
 
@@ -19,7 +19,7 @@ int binary_tree_balance(const binary_tree_t *tree)
  * Return: always
  */
 
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_he(const binary_tree_t *tree)
 {
 	size_t lef = 0, rai = 0;
 
---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+/**
+ * is_full_recursive - func
+ * @tree: val
+ * Return: always
+ */
+
+int recursive(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		if ((tree->left && !tree->right) ||
+		    (!tree->left && tree->right) ||
+		   recursive(tree->left) == 0 ||
+		   recursive(tree->right) == 0)
+			
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * binary_tree_is_full - func
+ * @tree: val
+ * Return: always
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		return (recursive(tree));
+	}
+	return (0);
+}
